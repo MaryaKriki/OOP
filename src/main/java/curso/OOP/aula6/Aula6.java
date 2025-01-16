152cfaf5d0e9426367b21f502dde47efc6746d50
MaryaKriki <mariae.kriki@gmail.com>
2025-01-16 14:05:59 -0300

    encapsulation

---
+++ src/main/java/curso/OOP/aula6/Aula6.java
@@ -0,0 +1,12 @@
+package curso.OOP.aula6;
+
+public class Aula6 {
+    public static void main(String[] args) {
+        ControleRemoto cm1 = new ControleRemoto();
+        cm1.ligar();
+        cm1.abrirMenu();
+        cm1.maisVolume();
+        cm1.play();
+        cm1.fecharMenu();
+    }
+}
